041c1f69809500d3078684ec8bd992929b57bb85
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-02-16 12:20:22 +0000

    Add print arr and print list fn

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,7 +14,7 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int tmp;
 
-	if i((!size && !array) || size < 2)
+	if (!(size && array) || size < 2)
 		return;
 
 	for (i = 1; i < size; i++)
