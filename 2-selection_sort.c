f438e151cb7164051fc261c2ed789d1eeca305c6
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-02-16 12:40:54 +0000

    Fix fn

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,6 +1,23 @@
 #include "sort.h"
 
 /**
+ * swap_ints - swaps two integers.
+ * @n1: The first integer.
+ * @n2: The second integer.
+ *
+ * Return: Void.
+ */
+
+void swap_ints(int *n1, int *n2)
+{
+	int tmp;
+
+	tmp = *n1;
+	*n1 = *n2;
+	*n2 = tmp;
+}
+
+/**
  * selection_sort - sorts an array of integers in
  * ascending order using the Selection sort algorithm.
  * @array: The given array.
@@ -11,26 +28,22 @@
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, k;
-	int tmp;
+	size_t i, j;
+	int *tmp;
 
 	if (!size || !array || size < 2)
 		return;
 
 	for (i = 1; i < size; i++)
 	{
-		for (j = size - 1, k = i + 1; j > i; j--)
-		{
-			if (array[j] < array[k])
-				k = j;
-		}
+		tmp = array + i;
 
-		if (array[i] > array[k])
-		{
-			tmp = array[i];
-			array[i] = array[k];
-			array[k] = tmp;
+		for (j = i + 1; j < size; j++)
+			tmp = (array[j] < *tmp) ? (array + j) : min;
 
+		if ((array + i) != tmp)
+		{
+			swap_ints(array + i, tmp)
 			print_array(array, size);
 		}
 	}
